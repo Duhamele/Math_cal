2a7254ab3c0f06245e00144cccb11df2f66010c4
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-04-19 23:30:40 +0200

    Modification sur randint et ajoute Bernoulli Randint:     -correction avec maintenant r in [a,b]     -mise des commentaire Bernoulli:     -Implémentation     -mise des commentaire

--- Calculmentale/MC_tool.cpp
+++ Calculmentale/MC_tool.cpp
@@ -1,10 +1,14 @@
 #include "MC_tool.h"
-#include <cstdlib>
+#include <stdlib.h>
 
 
 int MC_T_randint(int a, int b){
     if(a==b){return a;}
-    else{if(a<b){return rand()%(b-a)+a;}
-    else{return rand()%(a-b)+b;}}
+    else{if(a<b){return rand()%(b-a+1)+a;}
+    else{return rand()%(a-b+1)+b;}}
 }
-
+int MC_T_Bernoulli(float p){
+    float z =rand()/RAND_MAX;
+    if(z>p){return 0;}
+    else{return 1;}
+}
--- Calculmentale/MC_tool.h
+++ Calculmentale/MC_tool.h
@@ -1,7 +1,21 @@
 #ifndef _MC_TOOL_H_
 #define _MC_TOOL_H_
 
-
+/**
+ * @brief renvoie un nombre random entre a et b selon une loi uniforme
+ * @param a int, l'entier inferieur
+ * @param b int, l'entier supérieur
+ * @return int ,le nombre random
+ * @author Elie Duhamel
+ * @warning si a>b, la fonction inverse a et b
+*/
 int MC_T_randint(int a,int b);
+/**
+ * @brief simule une loi de Bernoulli
+ * @param p float, le paramétre de bernoulli
+ * @return int, 0 ou 1
+ * @warning if p>1, return 1,if p<0, return 0
+*/
+int MC_T_Bernoulli(float p);
 
 #endif
