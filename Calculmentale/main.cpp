2c415e624d2fddd6151243a5cb1ae0d110486d93
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-04-24 11:13:16 +0200

    Commencement de l'analyser de commande Calculmentale/main.cpp:     -MC_M_Analyse_entre debut Calculmentale/main.h:     -MC_M_Commande debut

--- Calculmentale/main.cpp
+++ Calculmentale/main.cpp
@@ -1,14 +1,40 @@
 #define Version "1.0"
+#define _NAME_ "Obacal"
 #include <stdio.h>
 #include <string.h>
 #include "../Calculmentale/main.h"
+#include<iostream>
 
-int main(){
+int main(int argc, char *argv[]){
     MC_M_Version();
+    
     return 0;
 
 }
 void MC_M_Version(){
-    printf("Obacal\n");
+    printf("%s\n",_NAME_);
     printf("Version : %s\n",Version);
+}
+void MC_M_Analyse_entre(int argc, char *argv[]){
+    int i=0;
+    while (i<argc)
+    {
+        MC_M_Commande arg=MC_M_Analyse_argument(argv[i]);
+        switch (arg)
+        {
+        case VERSION :
+            MC_M_Version();
+            i++;
+            break;
+        case HELP:
+            i++;
+            break;
+        
+        default:
+            printf("Message inconpris");
+            i=argc;
+            break;
+        }
+    }
+    
 }
--- Calculmentale/main.h
+++ Calculmentale/main.h
@@ -3,4 +3,7 @@
 */
 void MC_M_Version();
 
-int main();
+int main(int argc, char *argv[]);
+void MC_M_Analyse_entre(int argc, char *argv[]);
+enum MC_M_Commande{VOID,VERSION,HELP};
+MC_M_Commande MC_M_Analyse_argument(char*);
